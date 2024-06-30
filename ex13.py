980a0afb2b0390b587a34bca9a22e7fd53b0d410
kahung33 <kahung930809@gmail.com>
2024-06-30 20:19:42 +0800

    adding_ex26

--- ex13.py
+++ ex13.py
@@ -1,6 +1,6 @@
 from sys import argv
 # read the WYSS section for how to run this
-script, first, second, third = argv
+script, first, second, third ,fifth = argv
 
 forth = input("forth input")
 
@@ -9,3 +9,4 @@ print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
 print("Your forth variable is:", forth)
+print("Your fifth variable is:", fifth)
